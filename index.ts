08bd03477cf81d0aad1c7e6a1358b95b86f93149
adiyakhuu1 <my.as.glpzghoo@gmail.com>
2025-02-18 19:25:00 +0800

    zz

--- index.ts
+++ index.ts
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN,
+    origin: [process.env.CORS_ORIGIN!, process.env.CORS_ORIGIN2!],
     credentials: true,
   })
 );
